b1325c7dc4dc6d2ed676b8a3e169c82d711d489e
Du Yue <duyue36@gmail.com>
2017-05-09 18:02:41 +0200

    CatlikeCoding_Object Pools

---
+++ Assets/Scripts/Stuff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(MeshRenderer))]
+public class Stuff : PooledObject
+{
+
+    public Rigidbody Body { get; private set; }
+
+    MeshRenderer[] meshRenderers;
+
+    public void SetMaterial(Material m)
+    {
+        for (int i = 0; i < meshRenderers.Length; i++)
+        {
+            meshRenderers[i].material = m;
+        }
+    }
+
+    void Awake()
+    {
+        Body = GetComponent<Rigidbody>();
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    void OnTriggerEnter(Collider enteredCollider)
+    {
+        if (enteredCollider.CompareTag("Kill Zone"))
+        {
+            ReturnToPool();
+        }
+    }
+}
---
+++ Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+
+public class StuffSpawner : MonoBehaviour
+{
+    [System.Serializable]
+    public struct FloatRange
+    {
+
+        public float min, max;
+
+        public float RandomInRange
+        {
+            get
+            {
+                return Random.Range(min, max);
+            }
+        }
+    }
+    public Material stuffMaterial;
+    public Stuff[] stuffPrefabs;
+
+    float timeSinceLastSpawn;
+
+    public float velocity;
+
+    public FloatRange timeBetweenSpawns, scale, randomVelocity, angularVelocity;
+
+    float currentSpawnDelay;
+
+    void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= currentSpawnDelay)
+        {
+            timeSinceLastSpawn -= currentSpawnDelay;
+            currentSpawnDelay = timeBetweenSpawns.RandomInRange;
+            SpawnStuff();
+        }
+    }
+
+    void SpawnStuff()
+    {
+        Stuff prefab = stuffPrefabs[Random.Range(0, stuffPrefabs.Length)];
+        Stuff spawn = Instantiate<Stuff>(prefab);
+        //Stuff spawn = prefab.getP
+
+        spawn.transform.localPosition = transform.position;
+        spawn.transform.localScale = Vector3.one * scale.RandomInRange;
+        spawn.transform.localRotation = Random.rotation;
+
+        spawn.Body.velocity = transform.up * velocity + Random.onUnitSphere * randomVelocity.RandomInRange;
+        spawn.Body.angularVelocity = Random.onUnitSphere * angularVelocity.RandomInRange;
+
+        //spawn.GetComponent<MeshRenderer>().material = stuffMaterial;
+        spawn.SetMaterial(stuffMaterial);
+    }
+}
